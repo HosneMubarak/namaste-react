e71b732d8a9e6efb1876bf3381b7204c4f2bd69e
Hosne Mubarak <h.m.tasmir.cse@gmail.com>
2025-05-11 22:17:31 +0600

    Laying the foundation.

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hi I am h1 Tag."),
-    React.createElement("h2", {}, "I am ag 2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hi I am h1 Tag."),
-    React.createElement("h2", {}, "I am ag 2"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const Title = () => <h1 className="title">Namaste react JSX</h1>;
+
+const title = <h1>Title</h1>;
+
+const HeadingComponent = () => {
+  return (
+    <div className="container">
+      {title}
+      <h1 className="heading">Wellcome to namaste react.</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
